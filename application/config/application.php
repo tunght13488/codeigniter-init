f60a9ee420d5aca4635fd50eb7f784e90277bfb0
Ha The Tung <tunght13488@gmail.com>
2012-06-13 01:50:12 +0700

    add base controller

--- application/config/application.php
+++ application/config/application.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['application_foo'] = 'bar';
+$config['application_maintainance_mode'] = FALSE;
 
 /* End of file application.php */
 /* Location: ./application/config/application.php */
