35902c820cf184f969112a1124dcea39c022a6d5
Ha The Tung <tunght13488@gmail.com>
2012-06-13 00:40:31 +0700

    import html5boilerplate and integrate with template

--- ci_chmod.sh
+++ ci_chmod.sh
@@ -1,7 +1,11 @@
+chmod 0700 ./ci_chmod.sh ./ci_recess.sh
 find ./application -type f -exec chmod 0644 {} +
 find ./application -type d -exec chmod 0755 {} +
 find ./system -type f -exec chmod 0644 {} +
 find ./system -type d -exec chmod 0755 {} +
 find ./database -type f -exec chmod 0644 {} +
 find ./database -type d -exec chmod 0755 {} +
-chmod 0644 .gitignore index.php license.txt README.md
+find ./asset -type f -exec chmod 0644 {} +
+find ./asset -type d -exec chmod 0755 {} +
+find . -regex '.*\.htaccess.*' -type f -exec chmod 0755 {} +
+chmod 0644 .gitignore index.php license.txt README.md crossdomain.xml humans.txt robots.txt 404.html
---
+++ ci_recess.sh
@@ -0,0 +1,2 @@
+recess --compress asset/less/style.less > asset/css/style.min.css
+recess --compile asset/less/style.less > asset/css/style.css
