1c8187ea22f8555016680ea1c30db328b6b6699f
Ha The Tung <tunght13488@gmail.com>
2012-06-12 23:04:43 +0700

    update base config

--- application/config/database.php
+++ application/config/database.php
@@ -49,20 +49,21 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['port']     = 3306;
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'admin';
+$db['default']['database'] = 'codeigniter-init';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
+$db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
 $db['default']['char_set'] = 'utf8';
-$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['dbcollat'] = 'utf8_unicode_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
-$db['default']['stricton'] = FALSE;
+$db['default']['stricton'] = TRUE;
 
 
 /* End of file database.php */
---
+++ application/language/english/application_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['application_title'] = 'CodeIgniter Base Project';
+
+/* End of file application_lang.php */
+/* Location: ./application/language/english/application_lang.php */
